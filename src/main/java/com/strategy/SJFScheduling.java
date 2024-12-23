30405647349701bdb770b2f349a5389d49dffe29
a-ghandour <123655005+a-ghandour@users.noreply.github.com>
2024-12-23 07:01:40 +0200

    adding the correct formula for the aging to solve starvation

--- src/main/java/com/strategy/SJFScheduling.java
+++ src/main/java/com/strategy/SJFScheduling.java
@@ -12,18 +12,22 @@ public class SJFScheduling implements SchedulingStrategy {
         processes.sort(Comparator.comparingInt(p -> p.getArrivalTime()));
         int n = processes.size();
         List<CPUProcess> readyQueue = new ArrayList<>();
+        List<CPUProcess> scheduledProcesses = new ArrayList<>();
         int currentTime = 0, completed = 0, cur = 0;
-        while (completed < n) {while (cur < n && processes.get(cur).getArrivalTime() <= currentTime) {
+        while (completed < n) {
+            while (cur < n && processes.get(cur).getArrivalTime() <= currentTime) {
                 readyQueue.add(processes.get(cur));
                 cur++;
             }
-            for (CPUProcess process : readyQueue)
-                process.setPriority(process.getPriority() + 1);
+            for (CPUProcess process : readyQueue) {
+                process.setWaitingTime(currentTime - process.getArrivalTime());
+            }
 
             // Select the process with the shortest burst time or highest priority
             readyQueue.sort((p1, p2) -> {
-                if (p1.getBurstTime() == p2.getBurstTime())
-                    return Integer.compare(p2.getPriority(), p1.getPriority());
+                int prr1 = p1.getWaitingTime() * p2.getBurstTime(), prr2 = p2.getWaitingTime() * p1.getBurstTime();
+                if (prr1 != prr2)
+                    return Integer.compare(prr2, prr1);
                 return Integer.compare(p1.getBurstTime(), p2.getBurstTime());
             });
 
@@ -33,13 +37,14 @@ public class SJFScheduling implements SchedulingStrategy {
                 currentProcess.setTurnAroundTime(currentTime - currentProcess.getArrivalTime());
                 currentProcess.setWaitingTime(currentProcess.getTurnAroundTime() - currentProcess.getBurstTime());
                 completed++;
-            }
-            else {
+                scheduledProcesses.add(currentProcess);
+                currentTime += contextSwitchingTime;
+            } else {
                 // If no process is ready, increment time
                 currentTime++;
             }
         }
-        return processes;
+        return scheduledProcesses;
     }
 
 }
